08f7909ba9e1c6dc70f900a601ef700150196cea
sucupirap <pedro41julio@gmail.com>
2026-01-10 22:40:18 -0300

    comments

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,18 @@
 from sqlalchemy import Boolean, Integer, String
+# Atributo da classe será mapeado para uma coluna do db
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
+    # ID é mapeado na coluna do DB
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    # Garante que dois users nao tenham mesmo e-mail
     email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    # Armazena a senha em formato HASH
     hashed_password: Mapped[str] = mapped_column(String)
+    # Boolean que controla permissoes
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
 
     orders = relationship("Order", back_populates="user")
